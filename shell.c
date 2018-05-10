cdee42a418763ac3b1f0aa8858494c30bfdc8eea
ffoxsake <39161229+ffoxsake@users.noreply.github.com>
2018-05-10 21:15:51 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -7,18 +7,18 @@
 #include <sys/wait.h>
 
 
-
-
-
-
 int main (void){
+    
     char *input_buf = malloc(300);
     char prompt[] = "./>\0";
+	char cwd[1024];
 
     while(1){
+		if (getcwd(cwd, sizeof(cwd)) != NULL){
+			printf("%s", cwd);
+		}
         printf("%s", prompt);
         input_buf = fgets(input_buf, 299, stdin);
         printf("%s\n", input_buf);
     }
-
 }
